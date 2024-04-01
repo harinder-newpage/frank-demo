cfd016c71f9aac6b133232e88b95e79f45605ded
Harinder Singh <harinder.singh@pfizer.com>
2024-04-01 12:12:06 +0530

    V2

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -16,27 +16,21 @@ export default async function decorate(block) {
   // decorate footer DOM
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  footer.classList.add('container', 'text-center');
 
-  const footerBarContainer = footer.querySelector('.footer-bar-container');
-  footerBarContainer.classList.add('container', 'text-center');
+  const footerRows = Array.from(footer.children[0].children);
 
-  const footerBar = footer.querySelector('.footer-bar');
-  footerBar.classList.add('footer-items');
-  const footerRows = Array.from(footerBar.children);
-  footerRows.forEach((row) => {
+  footerRows.forEach(row => {
     row.classList.add('row', 'footer-items');
-    const footerCol = Array.from(row.children);
-    footerCol.forEach((col) => {
+    Array.from(row.children).forEach(col => {
       col.classList.add('col-md-2', 'footer-item');
-    })
-  })
-
-  const footerBarBottom = footer.querySelector('.footer-bar-bottom');
-  const footerBottomRows = Array.from(footerBarBottom.children);
-  footerBottomRows.forEach((row) => {
+    });
+  });
+  const footerBottomRows = Array.from(footer.children[1].children);
+  footerBottomRows.forEach(row => {
     row.classList.add('row', 'py-5');
     row.children[0].classList.add('col-lg-2', 'col-md-2', 'text-lg-end', 'text-start', 'footer-bar-bottom', 'left');
     row.children[1].classList.add('col-lg-7', 'col-md-7', 'text-start', 'footer-bar-bottom', 'right');
-  })
+  });
   block.append(footer);
 }
--- blocks/header/header.js
+++ blocks/header/header.js
@@ -21,57 +21,46 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
   const navSection = nav.querySelector('.section');
   navSection.classList.add('container-fluid');
-  const hamburgerHTML = `<a class="navbar-brand" href="#"></a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>`;
-  navSection.insertAdjacentHTML('afterbegin', hamburgerHTML)
+  navSection.insertAdjacentHTML('afterbegin', `<a class="navbar-brand" href="#"></a><button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>`);
+
   const siteNavbar = nav.querySelector('.default-content-wrapper');
   siteNavbar.classList.add('offcanvas', 'offcanvas-start');
   siteNavbar.id = 'offcanvasNavbar';
-  siteNavbar.setAttribute('tabindex', -1)
+  siteNavbar.setAttribute('tabindex', -1);
   siteNavbar.setAttribute('aria-labelledby', 'offcanvasNavbarLabel');
-  const menuHeaderHTML = `<div class="offcanvas-header">
-  <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><img src="./images/Know-Pneumonia-Logo.png" class="img-fluid" alt="Know Pneumonia Image"></h5>
-  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-</div>`
-  siteNavbar.insertAdjacentHTML('afterbegin', menuHeaderHTML);
-  const itemsArray = Array.from(siteNavbar.children);
-  itemsArray.forEach(function (item) {
-    if (item.nodeName === 'UL') {
-      var offcanvasBody = document.createElement('div');
-      offcanvasBody.classList.add('offcanvas-body');
-      var ulClone = item.cloneNode(true);
-      offcanvasBody.appendChild(ulClone);
-      item.parentNode.replaceChild(offcanvasBody, item);
-      const ulMenu = offcanvasBody.children[0]
-      ulMenu.classList.add('navbar-nav', 'justify-content-center', 'flex-grow-1');
-      var listItem = Array.from(ulMenu.children);
-      listItem.forEach(function (subItem) {
-        subItem.classList.add('nav-item', 'main-menu', 'dropdown');
-        const aTag = subItem.querySelectorAll('a');
-        aTag.forEach((a) => {
-          a.classList.add('nav-link');
-          a.setAttribute('aria-current', 'page');
-          const subMenu = subItem.querySelector('ul');
-          if (subMenu) {
-            subMenu.classList.add('dropdown-menu')
-            a.setAttribute('role', 'button');
-            a.setAttribute('data-bs-toggle', 'dropdown');
-            a.setAttribute('aria-expanded', 'false');
-            const aTagChild = subMenu.querySelectorAll('a');
-            aTagChild.forEach((subItemNew) => {
-              subItemNew.classList.add('dropdown-item', 'text-wrap');
-            })
-            const liTagChild = subMenu.querySelectorAll('li');
-            liTagChild.forEach((subItemNew) => {
-              subItemNew.classList.add('nav-item', 'sub-menu');
-            })
-          }
-        })
+  siteNavbar.insertAdjacentHTML('afterbegin', `<div class="offcanvas-header"><h5 class="offcanvas-title" id="offcanvasNavbarLabel"><img src="./images/Know-Pneumonia-Logo.png" class="img-fluid" alt="Know Pneumonia Image"></h5><button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button></div>`);
 
-      })
-    }
+  Array.from(siteNavbar.querySelectorAll('.default-content-wrapper > ul')).forEach(item => {
+    const offcanvasBody = document.createElement('div');
+    offcanvasBody.classList.add('offcanvas-body');
+    const ulClone = item.cloneNode(true);
+    offcanvasBody.appendChild(ulClone);
+    item.parentNode.replaceChild(offcanvasBody, item);
+
+    const ulMenu = offcanvasBody.children[0];
+    ulMenu.classList.add('navbar-nav', 'justify-content-center', 'flex-grow-1');
+
+    Array.from(ulMenu.children).forEach(subItem => {
+      subItem.classList.add('nav-item', 'main-menu', 'dropdown');
+      const aTag = subItem.querySelectorAll('a');
+      aTag.forEach(a => {
+        a.classList.add('nav-link');
+        a.setAttribute('aria-current', 'page');
+        const subMenu = subItem.querySelector('ul');
+        if (subMenu) {
+          subMenu.classList.add('dropdown-menu');
+          a.setAttribute('role', 'button');
+          a.setAttribute('data-bs-toggle', 'dropdown');
+          a.setAttribute('aria-expanded', 'false');
+          Array.from(subMenu.querySelectorAll('a')).forEach(subItemNew => {
+            subItemNew.classList.add('dropdown-item', 'text-wrap');
+          });
+          Array.from(subMenu.querySelectorAll('li')).forEach(subItemNew => {
+            subItemNew.classList.add('nav-item', 'sub-menu');
+          });
+        }
+      });
+    });
   });
   // My Code From Till Here
   block.append(nav);
